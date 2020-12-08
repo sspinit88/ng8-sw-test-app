0bbf5f1d93d743574a288548023881603705d4bb
Sergey Pashnin <sspinit88@gmail.com>
2020-12-08 12:05:57 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng8-sw-test-app';
+export class AppComponent implements OnInit {
+  array: number[] = [0, 1, 2];
+
+  constructor(
+    private swUpdate: SwUpdate,
+  ) {
+  }
+
+  ngOnInit(): void {
+
+    // TODO проверяем поддерживает ли браузер sw
+    if (this.swUpdate.isEnabled) {
+      this.checkVersion();
+    }
+
+  }
+
+  // при первой загрузке sw узнает о том есть ли новая версия, при второй - скачивает новую версию
+  checkVersion() {
+    // TODO проверка на наличие актуальной версии
+    this.swUpdate
+      .available
+      .subscribe(
+        () => {
+          if (confirm('Доступна новая версия приложения. Загрузить новую версию?')) {
+            window.location.reload();
+          }
+        }
+      );
+  }
+
 }
+
